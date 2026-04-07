ec3a109c80aa25140a5db186dd5fd596a7d2dc91
cdurken7-wq <cdurken7@gmail.com>
2026-04-06 23:34:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.'));
 
 const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
 const PLAID_SECRET = process.env.PLAID_SECRET;
